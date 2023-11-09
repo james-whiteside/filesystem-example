8e272be6eec01f4e9c2d003aab2f8b3e424a09b7
James Whiteside <james.whiteside@vaticle.com>
2023-11-09 09:41:05 +0000

    Fixed indent formatting.

--- postgresql/schema.sql
+++ postgresql/schema.sql
@@ -3,47 +3,47 @@ CREATE TABLE users (
 );
 
 CREATE TABLE admins (
-	email TEXT PRIMARY KEY REFERENCES users(email)
+    email TEXT PRIMARY KEY REFERENCES users(email)
 );
 
 CREATE TABLE user_groups (
-	name TEXT PRIMARY KEY
+    name TEXT PRIMARY KEY
 );
 
 CREATE TABLE resources (
-	id TEXT PRIMARY KEY
+    id TEXT PRIMARY KEY
 );
 
 CREATE TABLE files (
-	path TEXT PRIMARY KEY REFERENCES resources(id)
+    path TEXT PRIMARY KEY REFERENCES resources(id)
 );
 
 CREATE TABLE ownerships (
-	id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY
 );
 
 CREATE TABLE resource_ownerships (
-	id INT PRIMARY KEY REFERENCES ownerships(id)
+    id INT PRIMARY KEY REFERENCES ownerships(id)
 );
 
 CREATE TABLE user_of_resource_ownerships (
-	id INT PRIMARY KEY REFERENCES resource_ownerships(id),
-	user_id TEXT REFERENCES users(email),
-	resource_id TEXT REFERENCES resources(id)
+    id INT PRIMARY KEY REFERENCES resource_ownerships(id),
+    user_id TEXT REFERENCES users(email),
+    resource_id TEXT REFERENCES resources(id)
 );
 
 CREATE TABLE user_group_of_resource_ownerships (
-	id INT PRIMARY KEY REFERENCES resource_ownerships(id),
-	user_group_id TEXT REFERENCES user_groups(name),
-	resource_id TEXT REFERENCES resources (id)
+    id INT PRIMARY KEY REFERENCES resource_ownerships(id),
+    user_group_id TEXT REFERENCES user_groups(name),
+    resource_id TEXT REFERENCES resources (id)
 );
 
 CREATE TABLE group_ownerships (
-	id INT PRIMARY KEY REFERENCES ownerships(id)
+    id INT PRIMARY KEY REFERENCES ownerships(id)
 );
 
 CREATE TABLE admin_of_group_ownerships (
-	id INT PRIMARY KEY REFERENCES group_ownerships(id),
-	admin_id TEXT REFERENCES admins(email),
-	user_group_id TEXT REFERENCES user_groups(name)
+    id INT PRIMARY KEY REFERENCES group_ownerships(id),
+    admin_id TEXT REFERENCES admins(email),
+    user_group_id TEXT REFERENCES user_groups(name)
 );
