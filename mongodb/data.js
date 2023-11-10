4f0ba02669662b5e31ecb440579fe674ec04e5dd
James Whiteside <james.whiteside@vaticle.com>
2023-11-10 11:22:08 +0000

    Updated MongoDB example.

--- mongodb/data.js
+++ mongodb/data.js
@@ -1,35 +1,35 @@
 // Instantiate an admin with the specified email.
-db.users.insert(
-    { "email": "cedric@vaticle.com", "user_type": "admin" }
-)
+db.users.insert( {
+    "email": "cedric@vaticle.com",
+    "user_type": "admin"
+} )
 
 // Instantiate a user with the specified email.
-db.users.insert(
-    { "email": "jimmy@vaticle.com", "user_type": "user" }
-)
+db.users.insert( {
+    "email": "jimmy@vaticle.com",
+    "user_type": "user"
+} )
 
 // Instantiate a user group with the specified name, and assign the admin Cedric as its owner.
-db.user_groups.insert(
-    {
-        "name": "engineers",
-        "owner": { "type": "admin", "id": "cedric@vaticle.com" }
-    }
-)
+owner_id = db.users.find({ "email": "cedric@vaticle.com" }).next()["_id"]
+db.groups.insert( {
+    "name": "engineers",
+    "group_type": "user_group",
+    "owner": owner_id
+} )
 
 // Instantiate a file with the specified path, and assign the user Jimmy as its owner.
-db.resources.insert(
-    {
-        "path": "/jimmy/benchmark-results.xlsx",
-        "resource_type": "file",
-        "owner": { "type": "user", "id": "jimmy@vaticle.com" }
-    }
-)
+owner_id = db.users.find({ "email": "jimmy@vaticle.com" }).next()["_id"]
+db.resources.insert( {
+    "path": "/jimmy/benchmark-results.xlsx",
+    "resource_type": "file",
+    "owner": owner_id
+} )
 
 // Instantiate a file with the specified path, and assign the Engineers group as its owner.
-db.resources.insert(
-    {
-        "path": "/vaticle/feature-roadmap.pdf",
-        "resource_type": "file",
-        "owner": { "type": "user_group", "id": "engineers" }
-    }
-)
+owner_id = db.groups.find({ "name": "engineers" }).next()["_id"]
+db.resources.insert( {
+    "path": "/vaticle/feature-roadmap.pdf",
+    "resource_type": "file",
+    "owner": owner_id
+} )
--- mongodb/queries.js
+++ mongodb/queries.js
@@ -1,49 +1,122 @@
 // Retrieve the ID of the feature roadmap's owner.
-db.resources.find(
-    { "path": "/vaticle/feature-roadmap.pdf" },
-    { "_id": false, "owner_id": "$owner.id" }
-)
-
+db.resources.aggregate( [
+    { "$match": { "path": "/vaticle/feature-roadmap.pdf" } },
+    { "$lookup": {
+        "from": "users",
+        "localField": "owner",
+        "foreignField": "_id",
+        "as": "user_owners"
+    } },
+    { "$lookup": {
+        "from": "groups",
+        "localField": "owner",
+        "foreignField": "_id",
+        "as": "user_group_owners"
+    } },
+    { "$addFields": { "owners": { "$concatArrays": [ "$user_owners", "$user_group_owners" ] } } },
+    { "$unwind": "$owners" },
+    { "$project": {
+        "_id": false,
+        "owner_id": { "$switch": { "branches": [
+            { "case": { "$eq": [ "$owners.user_type", "user" ] }, "then": "$owners.email" },
+            { "case": { "$eq": [ "$owners.user_type", "admin" ] }, "then": "$owners.email" },
+            { "case": { "$eq": [ "$owners.group_type", "user_group" ] }, "then": "$owners.name" }
+        ] } }
+    } }
+] )
 
 // Retrieve the IDs of all resources owned by Jimmy.
-db.resources.aggregate([
-    { "$match": { "owner.id": "jimmy@vaticle.com" } },
-    { "$project": { "_id": false, "resource_id": {
-        "$switch": { "branches": [
-                { "case": { "$eq": ["$resource_type", "file"] }, "then": "$path" }
-            ] }
-    } } }
-])
+db.users.aggregate( [
+    { "$match": { "email": "jimmy@vaticle.com" } },
+    { "$lookup": {
+        "from": "resources",
+        "localField": "_id",
+        "foreignField": "owner",
+        "as": "owned_resources"
+    } },
+    { "$unwind": "$owned_resources" },
+    { "$project": {
+        "_id": false,
+        "owner_id": { "$switch": { "branches": [
+            { "case": { "$eq": [ "$owned_resources.resource_type", "file" ] }, "then": "$owned_resources.path" }
+        ] } }
+    } }
+] )
 
 // Retrieve the type and ID of all objects in the filesystem.
-db.users.aggregate([
-    { "$project": { "_id": false, "object_type": "$user_type", "object_id": "$email" } },
-    { "$unionWith": { "coll": "user_groups", "pipeline": [
-        { "$project": { "_id": false, "object_type": "user_group", "object_id": "$name" } }
-    ] } },
-    { "$unionWith": { "coll": "resources", "pipeline": [
-        { "$project": { "_id": false, "object_type": "$resource_type", "object_id": "$path" } }
-    ] } }
-])
+db.users.aggregate( [
+    { "$unionWith": { "coll": "groups" } },
+    { "$unionWith": { "coll": "resources" } },
+    { "$addFields": {
+        "object_type": { "$switch": { "branches": [
+            { "case": { "$eq": [ "$user_type", "user" ] }, "then": "user" },
+            { "case": { "$eq": [ "$user_type", "admin" ] }, "then": "admin" },
+            { "case": { "$eq": [ "$group_type", "user_group" ] }, "then": "user_group" },
+            { "case": { "$eq": [ "$resource_type", "file" ] }, "then": "file" }
+        ] } }
+    } },
+    { "$project": {
+        "_id": false,
+        "object_type": true,
+        "object_id": { "$switch": { "branches": [
+            { "case": { "$eq": [ "$user_type", "user" ] }, "then": "$email" },
+            { "case": { "$eq": [ "$user_type", "admin" ] }, "then": "$email" },
+            { "case": { "$eq": [ "$group_type", "user_group" ] }, "then": "$name" },
+            { "case": { "$eq": [ "$resource_type", "file" ] }, "then": "$path" }
+        ] } }
+    } }
+] )
 
 // Retrieve the details of every ownership in the filesystem.
-db.user_groups.aggregate([
-    { "$project": {
-            "_id": false,
-            "ownership_type": "group_ownership",
-            "owned_type": "user_group",
-            "owned_id": "$name",
-            "owner_type": "$owner.type",
-            "owner_id": "$owner.id"
+db.groups.aggregate( [
+    { "$addFields": { "ownership_type": "group_ownership" } },
+    { "$unionWith": {
+        "coll": "resources",
+        "pipeline": [ { "$addFields": { "ownership_type": "resource_ownership" } } ]
     } },
-    { "$unionWith": { "coll": "resources", "pipeline": [
-        { "$project": {
-            "_id": false,
-            "ownership_type": "resource_ownership",
-            "owned_type": "$resource_type",
-            "owned_id": "$path",
-            "owner_type": "$owner.type",
-            "owner_id": "$owner.id"
-        } }
-    ] } }
-])
+    { "$lookup": {
+        "from": "users",
+        "localField": "owner",
+        "foreignField": "_id",
+        "as": "user_owners"
+    } },
+    { "$lookup": {
+        "from": "groups",
+        "localField": "owner",
+        "foreignField": "_id",
+        "as": "user_group_owners"
+    } },
+    { "$addFields": { "owners": { "$concatArrays": [ "$user_owners", "$user_group_owners" ] } } },
+    { "$unwind": "$owners" },
+    { "$addFields": {
+        "owned_type": { "$switch": { "branches": [
+            { "case": { "$eq": [ "$group_type", "user_group" ] }, "then": "user_group" },
+            { "case": { "$eq": [ "$resource_type", "file" ] }, "then": "file" }
+        ] } }
+    } },
+    { "$addFields": {
+        "owned_id": { "$switch": { "branches": [
+            { "case": { "$eq": [ "$group_type", "user_group" ] }, "then": "$name" },
+            { "case": { "$eq": [ "$resource_type", "file" ] }, "then": "$path" }
+        ] } }
+    } },
+    { "$addFields": {
+        "owner_type": { "$switch": { "branches": [
+            { "case": { "$eq": [ "$owners.user_type", "user" ] }, "then": "user" },
+            { "case": { "$eq": [ "$owners.user_type", "admin" ] }, "then": "admin" },
+            { "case": { "$eq": [ "$owners.group_type", "user_group" ] }, "then": "user_group" }
+        ] } }
+    } },
+    { "$project": {
+        "_id": false,
+        "ownership_type": true,
+        "owned_type": true,
+        "owned_id": true,
+        "owner_type": true,
+        "owner_id": { "$switch": { "branches": [
+            { "case": { "$eq": [ "$owners.user_type", "user" ] }, "then": "$owners.email" },
+            { "case": { "$eq": [ "$owners.user_type", "admin" ] }, "then": "$owners.email" },
+            { "case": { "$eq": [ "$owners.group_type", "user_group" ] }, "then": "$owners.name" }
+        ] } }
+    } }
+] )
