4cf66f365226eb3151957d662c2daee6b2d2a434
James Whiteside <james.whiteside@vaticle.com>
2023-11-10 11:23:04 +0000

    Updated MongoDB example.

--- mongodb/data.js
+++ mongodb/data.js
@@ -11,7 +11,7 @@ db.users.insert( {
 } )
 
 // Instantiate a user group with the specified name, and assign the admin Cedric as its owner.
-owner_id = db.users.find({ "email": "cedric@vaticle.com" }).next()["_id"]
+owner_id = db.users.find( { "email": "cedric@vaticle.com" } ).next()["_id"]
 db.groups.insert( {
     "name": "engineers",
     "group_type": "user_group",
@@ -19,7 +19,7 @@ db.groups.insert( {
 } )
 
 // Instantiate a file with the specified path, and assign the user Jimmy as its owner.
-owner_id = db.users.find({ "email": "jimmy@vaticle.com" }).next()["_id"]
+owner_id = db.users.find( { "email": "jimmy@vaticle.com" } ).next()["_id"]
 db.resources.insert( {
     "path": "/jimmy/benchmark-results.xlsx",
     "resource_type": "file",
@@ -27,7 +27,7 @@ db.resources.insert( {
 } )
 
 // Instantiate a file with the specified path, and assign the Engineers group as its owner.
-owner_id = db.groups.find({ "name": "engineers" }).next()["_id"]
+owner_id = db.groups.find( { "name": "engineers" } ).next()["_id"]
 db.resources.insert( {
     "path": "/vaticle/feature-roadmap.pdf",
     "resource_type": "file",
